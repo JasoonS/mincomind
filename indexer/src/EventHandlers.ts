18d030354d8ccdf88d7c69cb4073c21e644d08bb
denhampreen <denhampreen@gmail.com>
2024-07-13 00:52:53 +0200

    feat: track tournament in indexer (guessed events)

--- indexer/src/EventHandlers.ts
+++ indexer/src/EventHandlers.ts
@@ -1,9 +1,17 @@
-import { GameContract, PotContract, PlayerEntity } from "generated";
+import {
+  GameContract,
+  PotContract,
+  PlayerEntity,
+  TournamentEntity,
+} from "generated";
+
+const TournamentID = "mincomind";
+const gameDepositAmount = BigInt(1); // todo: set once its known
 
 // - event: GameStarted(address player)
-GameContract.GameStarted.handler(({ event, context }) => {
-  let player: PlayerEntity | undefined = context.Player.get(
-    event.params.player.toHex()
+GameContract.GameStarted.handler(async ({ event, context }) => {
+  let player: PlayerEntity | undefined = await context.Player.get(
+    event.params.player
   );
 
   // new player, so create
@@ -23,12 +31,28 @@ GameContract.GameStarted.handler(({ event, context }) => {
       active: true,
     });
   }
+
+  // create tournament if it doesn't exist
+  let tournament: TournamentEntity | undefined = await context.Tournament.get(
+    TournamentID
+  );
+  if (tournament === undefined) {
+    context.tournament.set({
+      id: TournamentID,
+      pot: gameDepositAmount,
+    });
+  } else {
+    context.tournament.set({
+      ...tournament,
+      pot: tournament.pot + gameDepositAmount,
+    });
+  }
 });
 
 // - event: GameOutcome(address player, uint32 points) - 0 points implies the player didn't solve the game
-GameContract.GameOutcome.handler(({ event, context }) => {
-  let player: PlayerEntity | undefined = context.Player.get(
-    event.params.player.toHex()
+GameContract.GameOutcome.handler(async ({ event, context }) => {
+  let player: PlayerEntity | undefined = await context.Player.get(
+    event.params.player
   );
 
   // this shouldnt be possible
@@ -45,9 +69,9 @@ GameContract.GameOutcome.handler(({ event, context }) => {
 });
 
 // - event: FundsWithdrawn(address player, uint32 amount)
-PotContract.FundsWithdrawn.handler(({ event, context }) => {
-  let player: PlayerEntity | undefined = context.Player.get(
-    event.params.player.toHex()
+PotContract.FundsWithdrawn.handler(async ({ event, context }) => {
+  let player: PlayerEntity | undefined = await context.Player.get(
+    event.params.player
   );
 
   // this shouldnt be possible
