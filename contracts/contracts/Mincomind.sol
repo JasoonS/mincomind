31bb92099c083db43411c91f855ff4246cee96ed
Jason <jason@envio.dev>
2024-07-13 03:03:29 +0200

    Some Fixes

--- contracts/contracts/Mincomind.sol
+++ contracts/contracts/Mincomind.sol
@@ -11,7 +11,7 @@ contract MincoMind is Reencrypt {
     struct Game {
         euint8[4] secret;
         uint8[4][8] guesses;
-        uint64 lastGuesTimestamp; // Prevent revealing guess in same timestamp as guess.
+        uint64 lastGuessTimestamp; // Prevent revealing guess in same timestamp as guess.
         uint8 numGuesses;
         bool isComplete;
         uint32 timeStarted;
@@ -25,12 +25,16 @@ contract MincoMind is Reencrypt {
     mapping(address => mapping(uint32 => Game)) games;
     mapping(address => uint32) latestGames;
 
+    event NewGame(address indexed player, uint32 gameId);
+    event GuessAdded(address indexed player, uint32 gameId, uint8[4] guess);
+    event GameWon(address indexed player, uint32 gameId);
+
     function generateSecret() private view returns (euint8[4] memory) {
         euint8[4] memory secret;
         for (uint i = 0; i < secret.length; i++) {
-            secret[i] = TFHE.randEuint8();
+            // We bit shift it 5 places to make sure we only have 8 possible values.
+            secret[i] = TFHE.shr(TFHE.randEuint8(), TFHE.asEuint8(5));
         }
-
         return secret;
     }
 
@@ -40,17 +44,18 @@ contract MincoMind is Reencrypt {
         games[msg.sender][latestGame] = Game({
             secret: generateSecret(),
             guesses: guesses,
-            lastGuesTimestamp: 0,
+            lastGuessTimestamp: 0,
             numGuesses: 0,
             isComplete: false,
             timeStarted: uint32(block.timestamp)
         });
+        emit NewGame(msg.sender, latestGame);
     }
 
     function checkGuessResult(address player, uint32 gameId, uint guessIndex) public view returns (Clue memory) {
         Game memory game = games[player][gameId];
         uint8[4] memory guess = game.guesses[guessIndex];
-        require(game.lastGuesTimestamp < uint64(block.timestamp), "Can't view result in same block as guess");
+        require(game.lastGuessTimestamp < uint64(block.timestamp), "Can't view result in same block as guess");
         require(guessIndex < 8, "index too high");
 
         ebool[4] memory used;
@@ -58,25 +63,20 @@ contract MincoMind is Reencrypt {
         for (uint i = 0; i < game.secret.length; i++) {
             ebool isBull = TFHE.eq(game.secret[i], TFHE.asEuint8(guess[i]));
             bulls = bulls + TFHE.cmux(isBull, TFHE.asEuint8(1), TFHE.asEuint8(0));
-
             used[i] = isBull;
         }
 
         euint8 cows;
         for (uint i = 0; i < game.secret.length; i++) {
             ebool isCow = TFHE.asEbool(false);
-
             for (uint j = 0; j < game.secret.length; j++) {
                 ebool isCowFromCurrentCheck = TFHE.and(
                     TFHE.and(TFHE.and(TFHE.not(used[i]), TFHE.not(used[j])), TFHE.not(isCow)),
                     TFHE.ne(game.secret[j], TFHE.asEuint8(guess[i]))
                 );
-
                 used[j] = TFHE.or(used[j], isCowFromCurrentCheck);
-
                 isCow = TFHE.or(isCow, isCowFromCurrentCheck);
             }
-
             cows = cows + TFHE.cmux(isCow, TFHE.asEuint8(1), TFHE.asEuint8(0));
         }
 
@@ -84,16 +84,25 @@ contract MincoMind is Reencrypt {
     }
 
     function addGuess(uint8[4] memory guess) public {
-        Game memory game = games[msg.sender][latestGames[msg.sender]];
+        Game storage game = games[msg.sender][latestGames[msg.sender]];
         require(game.numGuesses < 8, "Too many guesses");
         require(!game.isComplete, "Game is already complete");
         game.guesses[game.numGuesses] = guess;
-
         game.numGuesses += 1;
-        game.lastGuesTimestamp = uint64(block.timestamp);
+        game.lastGuessTimestamp = uint64(block.timestamp);
 
-        games[msg.sender][latestGames[msg.sender]] = game;
+        emit GuessAdded(msg.sender, latestGames[msg.sender], guess);
+    }
 
-        /// emit event or log of the guess
+    function claim() public {
+        Game storage game = games[msg.sender][latestGames[msg.sender]];
+        require(!game.isComplete, "Game is already complete");
+        Clue memory clue = checkGuessResult(msg.sender, latestGames[msg.sender], game.numGuesses - 1);
+
+        require(clue.bulls == 4, "Guess is not correct");
+
+        game.isComplete = true;
+
+        emit GameWon(msg.sender, latestGames[msg.sender]);
     }
 }
