6feda48870d5b89fe2e096c8ecab0a83cb489db7
Jason <jason@envio.dev>
2024-07-13 23:14:38 +0200

    Fix linux - add polyfills

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
     react({
       include: ["**/*.res.mjs"],
     }),
-  ],
+    nodePolyfills()
+  ]
 });
